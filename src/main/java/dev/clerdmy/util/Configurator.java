8f2b0e43aa14894eb4cb483f4eb1b762044f35ba
CLerdmy <podturov@gmail.com>
2025-04-18 22:08:01 +0300

    configuration

--- src/main/java/dev/clerdmy/util/Configurator.java
+++ src/main/java/dev/clerdmy/util/Configurator.java
@@ -3,12 +3,12 @@ package dev.clerdmy.util;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class Config {
+public class Configurator {
 
     private static final Properties properties = new Properties();
 
     static {
-        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+        try (InputStream input = Configurator.class.getClassLoader().getResourceAsStream("config.properties")) {
             properties.load(input);
         } catch (Exception e) {
             throw new RuntimeException(e);
