577f401daf6837ce5fa35dc4c2729acb8d8a4d6d
CLerdmy <podturov@gmail.com>
2025-04-18 22:07:30 +0300

    db connector

--- src/main/java/dev/clerdmy/util/DatabaseConnector.java
+++ src/main/java/dev/clerdmy/util/DatabaseConnector.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
 
-    private static final String URL = Config.get("db.url");
-    private static final String USER = Config.get("db.username");
-    private static final String PASSWORD = Config.get("db.password");
+    private static final String URL = Configurator.get("db.url");
+    private static final String USER = Configurator.get("db.username");
+    private static final String PASSWORD = Configurator.get("db.password");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
